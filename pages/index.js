62f8ba8d3fac83ff7efd09a0ac88cbb87c86ca67
Scar <71942531+NotScar@users.noreply.github.com>
2023-11-07 21:15:38 +0100

    feat: add meta tags :sparkles:

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,17 @@ export default function Search() {
             <Head>
                 <title>Steam to FACEIT</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                <meta name="description" content="Discover FaceIT Accounts Linked to Players' Steam Profiles | Uncover Elo, Win Stats, Ban History, and More!"/>
                 <link rel="shortcut icon" href="/favicon.png" type="image/x-icon"/>
+                <meta name="keywords" content="FACEIT, STEAM, FACEIT TO STEAM, FACEIT2STEAM, STEAM TO FACEIT, STEAM2FACEIT, FACEIT ACCOUNT"/>
+                <link rel="icon" href="/favicon.png" type="image/x-icon"/>
+
+                <meta property="og:title" content="Steam to FACEIT"/>
+                <meta property="og:type" content="website" />
+                <meta property="og:description" content="Discover FaceIT Accounts Linked to Players' Steam Profiles | Uncover Elo, Win Stats, Ban History, and More!"/>
+                <meta property="og:url" content="https://faceit.notscar.com"/>
+                <meta property="og:image" content="/favicon.png"/>
+                <meta property="og:image:type" content="image/png" />
             </Head>
             <Header/>
             <SearchBar searchAction={getPlayerInformation} isLoading={isLoading}/>
